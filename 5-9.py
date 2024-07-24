8f5121f5c408e288612fc2c78aac7b0383e83ddb
itc-s24011 <s24011@std.it-college.ac.jp>
2024-07-24 07:54:42 +0000

    ch4 6日目

---
+++ 5-9.py
@@ -0,0 +1,13 @@
+mountain_in_japan = {'fuji': 3776, 'kitadake': 3193, 'okuhodakadake': 3190}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[0])
+for key, val in mountain_in_japan_sorted:
+    print(key, val)
+mountain_in_japan = {'fuji': 3776, 'kitadake': 3193, 'okuhodakadake': 3190}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[1])
+for key, val in mountain_in_japan_sorted:
+    print(key, val)
+for key, val in reversed(mountain_in_japan_sorted):
+    print(key, val)
+mountain_in_japan = {'fiji':3776, 'kitadake': 3193, 'okuhodakadake': 3190, 'dummy': 0}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[1], reverse=True)
+print(mountain_in_japan_sorted)
